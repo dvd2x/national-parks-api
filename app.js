affbccf9080a22142066508b0ff30d9c46b3026f
David Piper <black.god1930@gmail.com>
2019-05-09 10:53:28 -0400

    fixed js so site can find state parks with state abbriviation

--- app.js
+++ app.js
@@ -32,11 +32,128 @@ function displayResults(responseJson) {
 }
 
 function getNationalParksInfo(query, maxResults = 10) {
+  const stateCodes = [
+    "AL",
+    "AK",
+    "AZ",
+    "AR",
+    "CA",
+    "CO",
+    "CT",
+    "DE",
+    "FL",
+    "GA",
+    "HI",
+    "ID",
+    "IL",
+    "IN",
+    "IA",
+    "KS",
+    "KY",
+    "LA",
+    "ME",
+    "MD",
+    "MA",
+    "MI",
+    "MN",
+    "MS",
+    "MO",
+    "MT",
+    "NE",
+    "NV",
+    "NH",
+    "NJ",
+    "NM",
+    "NY",
+    "NC",
+    "ND",
+    "OH",
+    "OK",
+    "OR",
+    "PA",
+    "RI",
+    "SC",
+    "SD",
+    "TN",
+    "TX",
+    "UT",
+    "VT",
+    "VA",
+    "WA",
+    "WV",
+    "WI",
+    "WY",
+    null
+  ];
+
+  const stateNames = [
+    "alabama",
+    "alaska",
+    "arizona",
+    "arkansas",
+    "california",
+    "colorado",
+    "connecticut",
+    "delaware",
+    "florida",
+    "georgia",
+    "hawaii",
+    "idaho",
+    "illinois",
+    "indiana",
+    "iowa",
+    "kansas",
+    "kentucky",
+    "louisiana",
+    "maine",
+    "maryland",
+    "massachusetts",
+    "michigan",
+    "minnesota",
+    "mississippi",
+    "missouri",
+    "montana",
+    "nebraska",
+    "nevada",
+    "new hampshire",
+    "new jersey",
+    "new mexico",
+    "new york",
+    "north carolina",
+    "north dakota",
+    "ohio",
+    "oklahoma",
+    "oregon",
+    "pennsylvania",
+    "rhode island",
+    "south carolina",
+    "south dakota",
+    "tennessee",
+    "texas",
+    "utah",
+    "vermont",
+    "virginia",
+    "washington",
+    "west virginia",
+    "wisconsin",
+    "wyoming"
+  ];
+
+  const stateName =
+    stateNames.indexOf(query.toLowerCase()) || stateCodes.length - 1;
+  const stateCode =
+    stateCodes.find(function(code) {
+      return code === query.toUpperCase();
+    }) || stateCodes[stateName];
   const params = {
     key: apiKey,
-    q: query,
     limit: maxResults - 1
   };
+  if (stateCode) {
+    params.stateCode = stateCode;
+  } else {
+    params.q = query;
+  }
   const queryString = formatQueryParams(params);
   const url = searchURL + "?" + queryString;
 
